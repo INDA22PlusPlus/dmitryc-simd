a4b53c4e2426ed0ec50beb236807b8a073f98cd0
Dmitry Chirin <chirin.dima@gmail.com>
2023-02-17 02:56:01 +0100

    attempt at optimizing simd, no progress

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,8 @@ fn add_vec3(u: &Vec3, v: &Vec3, result: &mut Vec3) {
     result.z = u.z + v.z;
 }
 
-fn add_vec3_optimized(u: &Vec3Optimized, v: &Vec3Optimized) -> Vec3Optimized {
-    Vec3Optimized {register: unsafe {_mm_add_ps(u.register, v.register)}}
+fn add_vec3_optimized(u: &Vec3Optimized, v: &Vec3Optimized, result: &mut Vec3Optimized){
+    result.register = unsafe {_mm_add_ps(u.register, v.register)};
 }
 
 fn main() {
@@ -30,21 +30,21 @@ fn main() {
     // Normal vectors
     let u = Vec3{x:1.0, y:2.0, z:3.0};
     let v = Vec3{x:4.0, y:5.0, z:6.0};
-    let mut w = Vec3{x:0.0, y:0.0, z:0.0};
+    let mut add_result_normal = Vec3{x:0.0, y:0.0, z:0.0};
 
     // Normal vectors benchmark
-    microbench::bench(&options, "add Vec3 normal", || add_vec3(&u, &v, &mut w));
+    microbench::bench(&options, "add Vec3 normal", || add_vec3(&u, &v, &mut add_result_normal));
 
-    println!("w: ({}, {}, {})", w.x, w.y, w.z);
+    // Should print: "w: (5, 7, 9)"
+    println!("w: ({}, {}, {})", add_result_normal.x, add_result_normal.y, add_result_normal.z);
 
     // Optimized vectors. Data represented as f32x4 in a _m128, where only the first 3 floats are
     // of interest, so the last one can is simply ignored. The order is flipped, the last value
     // is the most significant one.
     let a = Vec3Optimized{register: unsafe {_mm_set_ps(0.0, 3.0, 2.0, 1.0)}};
     let b = Vec3Optimized{register: unsafe {_mm_set_ps(0.0, 6.0, 5.0, 4.0)}};
-    let mut c = Vec3Optimized{register: unsafe {_mm_set_ps(0.0, 0.0, 0.0, 0.0)}};
+    let mut add_result_optimized = Vec3Optimized{register: unsafe {_mm_set_ps(0.0, 0.0, 0.0, 0.0)}};
 
     // Normal vectors benchmark
-    microbench::bench(&options, "add Vec3 optimized", || add_vec3_optimized(&a, &b));
-
+    microbench::bench(&options, "add Vec3 optimized", || add_vec3_optimized(&a, &b, &mut add_result_optimized));
 }
